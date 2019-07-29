a1834c7b8239683a3f38489a90797b0f6cd2f872
Amy <manage@rathcooledesign.com>
2019-07-29 18:01:03 -0400

    carousel

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,9 @@
 $( document ).ready(function() {
-
+  $('.carousel').slick({
+       adaptiveHeight: true;
+       autoplay: true;
+       arrows: false;
+       fade: true;
+       mobileFirst: true;
+   });
 });
