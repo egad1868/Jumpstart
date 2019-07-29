c02a2e33a88cccc05c95d7476ec0075f3d9f568e
Amy <manage@rathcooledesign.com>
2019-07-29 18:56:53 -0400

    disappear navbar

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,8 +2,22 @@ $( document ).ready(function() {
   $('.carousel').slick({
        adaptiveHeight: true,
        autoplay: true,
+       autoplaySpeed: 4000,
        arrows: false,
+       draggable: false,
        fade: true,
-       mobileFirst: true;
+       mobileFirst: true,
+       pauseOnFocus: false,
+       pauseOnHover: false,
    });
+
+   var userScroll = 0;
+   $('.navbar').scroll();
+   var userScrollUp = $(this).scrollTop() {
+     if ('userScrollUp + '-' + userScroll > 50') {
+    var navbarHeight = $('.navbar').css('height');
+    $('.navbar').animate({top: '-' + navbarHeight}, 150);
+
+  }
+   }
 });
